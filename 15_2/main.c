a5b5965775155db212267a38b66e232ad28d514e
Vipul Verma <66062033+vip02aug@users.noreply.github.com>
2024-09-22 19:58:52 +0530

    Introducing Headers and handler

--- 15_2/main.c
+++ 15_2/main.c
@@ -1,9 +1,12 @@
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
 
+volatile uint32_t dutyCycle = 50;  /* Initial duty cycle is set to 50% */
+volatile uint32_t count = 160;     /* Count value calculated using the 16MHz clock */
+volatile uint32_t pressTime = 0;   /* Stores how long the button has been held down */
+volatile uint8_t bp = 0;           /* Flag to detect if the button has been pressed */
 
-/**
- * main.c
- */
-int main(void)
-{
-	return 0;
-}
+void init(void);
+void Systick_Init(void);
+void GPIO_Handler(void);
+void Systick_Handler(void);
